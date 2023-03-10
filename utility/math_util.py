77f46b019a264b537cfa3c91bc61e7171caa088a
SFBB <anzhe990206@outlook.com>
2023-03-10 22:45:58 +0800

    Add rs_analysis math utility.

---
+++ utility/math_util.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+class math_util:
+    @staticmethod
+    @staticmethod
+    def divide_chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i: i+n]
+    @staticmethod
+    def rs_analysis(x, min_chunk_size):
+        if min_chunk_size < 2:
+            min_chunk_size = 2
+        N = len(x)
+        rs_series = []
+        n_series = []
+        # 1. The series is divided into chunks of chunk_size_list size
+        chunk_size = min_chunk_size
+        while chunk_size < len(x):
+            rs_n_list = []
+            for start_index in range(0, len(x) - chunk_size, chunk_size):
+                x_n = x[start_index : start_index + chunk_size]
+                z_t = np.cumsum(x_n - np.mean(x_n))
+                r_n = np.max(z_t) - np.min(z_t)
+                s_n = np.nanstd(x_n)
+                rs_n = 1.0
+                if not np.abs(s_n) < 0.0001:
+                    rs_n = np.divide(r_n, s_n)
+                rs_n_list.append(rs_n)
+            rs_series.append(np.nanmean(rs_n_list))
+            n_series.append(chunk_size)
+
+            # We increment index by 1 per sampling.
+            chunk_size = 2 * chunk_size
+
+        plt.plot(np.log(n_series), np.log(rs_series))
+        plt.plot(np.arange(np.log(n_series)[0], np.log(n_series)[-1]), 0.5 * np.arange(np.log(n_series)[0], np.log(n_series)[-1]))
+        # 3. calculate the Hurst exponent.
+        H, c = np.linalg.lstsq(
+            a=np.vstack((np.log(n_series), np.ones(len(n_series)))).T,
+            b=np.log(rs_series),
+            rcond=None
+        )[0]
+        # plt.plot(np.arange(np.log(n_series)[0], np.log(n_series)[-1]), H * np.arange(np.log(n_series)[0], np.log(n_series)[-1]) + c)
+        plt.show()
+
+        return H, c
+
+
+
+if __name__ == "__main__":
+    number = 1000
+    x = np.random.rand(number)
+    H, c = math_util.rs_analysis(x, 2)
+    print("H: {}, c: {}".format(H, c))
