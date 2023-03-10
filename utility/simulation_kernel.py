75ba9b3e322a108a60bc03013471392674f75d83
SFBB <anzhe990206@outlook.com>
2023-03-10 22:47:02 +0800

    Change name of variables.

--- utility/simulation_kernel.py
+++ utility/simulation_kernel.py
@@ -6,28 +6,28 @@ from stock_util import stock
 
 class simulation_kernel():
     def __init__(self):
-        self.__strategies = [] # list[strategy_base]
-        self.__stocks_info = {} # dict[str, stock]
+        self.__strategies__ = [] # list[strategy_base]
+        self.__stocks_info__ = {} # dict[str, stock]
 
     def add_strategy(self, strategy: strategy_base):
-        self.__strategies.append(strategy)
+        self.__strategies__.append(strategy)
 
     def initialize(self):
         self.__collect_strategies_stock_names()
 
     def __collect_strategies_stock_names(self):
-        for strategy in self.__strategies:
+        for strategy in self.__strategies__:
             stock_names = strategy.get_stock_names()
             for stock_name in stock_names:
-                self.__stocks_info[stock_name] = stock(stock_name)
+                self.__stocks_info__[stock_name] = stock(stock_name)
 
     def update_stock(self):
         pass
 
     def run(self):
         while True:
-            for strategy in self.__strategies:
-                strategy.tick(self.__stocks_info)
+            for strategy in self.__strategies__:
+                strategy.tick(self.__stocks_info__)
 
 if __name__ == "__main__":
     sk = simulation_kernel()
