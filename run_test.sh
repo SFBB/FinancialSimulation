45ff8799adf24da46c8d3ccbf5c701e69a709e03
SFBB <anzhe990206@outlook.com>
2025-12-30 23:00:47 +0800

    chore: Move test related files into test/ dir. Add run_test.sh script.

---
+++ run_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+source venv/bin/activate
+
+python3 -m test.test_realism
+python3 -m test.reproduce_issue
+python3 -m test.test_moutai
+python3 -m test.check_market_trend
+python3 -m test.test_data_loader
+python3 -m test.test_look_ahead_fix
